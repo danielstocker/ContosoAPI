3e8b142df4b77d5ecf3979429fa6579c67254894
Daniel Stocker <dasto@microsoft.com>
2017-10-11 21:15:38 +0200

    update AI and iKey var

--- ContosoAPI/Global.asax.cs
+++ ContosoAPI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace ContosoAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            TelemetryConfiguration.Active.InstrumentationKey = System.Web.Configuration.WebConfigurationManager.AppSettings["iKey"];
         }
     }
 }
